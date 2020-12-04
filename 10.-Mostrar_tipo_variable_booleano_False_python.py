26a517c58d62a04a3ab5aedc58939e22bb47c9b8
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 20:01:13 -0600

    Add files via upload

---
+++ 10.-Mostrar_tipo_variable_booleano_False_python.py
@@ -0,0 +1,10 @@
+print("Este programa muestra una variable de tipo booleano con valor False")
+print("También muestra el tipo de variable en pantalla")
+print('Como observación, el valor: "False" se debe escribir con la primera letra mayúscula')
+valorbooleano = False
+print("El valor de la variable es: ")
+print(valorbooleano)
+print(type(valorbooleano))
+print("Este programa fue escrito por Emilio Carcaño Bringas")
+
+# Este programa fue escrito por Emilio Carcaño Bringas
